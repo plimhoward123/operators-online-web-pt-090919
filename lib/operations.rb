701a1282194f28011cdcc842f8a7a3762e1e319a
Plimon Howard V <plimon.howard@gmail.com>
2019-09-10 21:58:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+	if (speed < 30 || speed > 60)
+		return false
+	else
+		return true
 end
 
 
 
 def not_safe?(speed)
-	
+	#return (speed < 30 || speed > 60) "dangriouse" : "safe"
 end
-	
-
-
